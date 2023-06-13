6f34e964c4c157bdb7084b41073c45e956dd940e
arikurumo <47628188+arikurumo@users.noreply.github.com>
2023-06-13 14:38:18 +0200

    Create sk.js

---
+++ langs/sk.js
@@ -0,0 +1,10 @@
+tinyMCE.addI18n('sk'
+               ,{'Tabs content': "Accordion - rolovací zoznam"
+                ,'Accordion content': "Obsah rolovacieho zoznamu"
+                ,'Number of tabs': "Počet záložiek v zozname"
+                ,'Close': "Zatvoriť"
+                ,'Save': "Vložiť"
+                ,'Change this header!': "Teraz zmeňte hlavičku záložky!"
+                ,"Content of panel. Let's go change it!": "Obsah záložky. Môžete ho začať editovať ;)"
+                }
+               );
